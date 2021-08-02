514756f14f2fa9b37ec943bdb3c236bf3251e1d9
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-08-02 14:31:32 +0700

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { KompasWwwSubmodulesSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  KompasWwwSubmodulesSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <kompas-www-submodules-sample />
+  </div>
+</template>
---
+++ kompas-www-submodules.d.ts
@@ -0,0 +1,7 @@
+import Vue, { PluginFunction, VueConstructor } from 'vue';
+
+
+declare const KompasWwwSubmodules: PluginFunction<any>;
+export default KompasWwwSubmodules;
+
+export const KompasWwwSubmodulesSample: VueConstructor<Vue>;
