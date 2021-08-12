f0b2b5027027e447f96c39c8d34706fe3a3dc475
elpranata <elgayudapranata@gmail.com>
2021-08-12 18:00:39 +0700

    single paragraph

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 // export { default as KompasWwwSubmodulesSample } from './kompas-www-submodules-sample.vue';
 export { default as KsmSingleTitle } from './single/Title.vue'
+export { default as KsmSingleParagraph } from './single/Paragraph.vue'
