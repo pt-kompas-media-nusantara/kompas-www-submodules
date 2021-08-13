b8ed37bf39d7aa29cbe9c51c5c20c7ad298c13ea
Hendy Dwi Putranto <hendy.putranto@kompas.com>
2021-08-13 14:20:47 +0700

    Credits dan Excerpt

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 // export { default as KompasWwwSubmodulesSample } from './kompas-www-submodules-sample.vue';
 export { default as KsmSingleTitle } from './single/Title.vue'
+export { default as KsmSingleExcerpt } from './single/Excerpt.vue'
+export { default as KsmSingleCredits } from './single/Credits.vue'
 export { default as KsmSingleParagraph } from './single/Paragraph.vue'
 export { default as KsmSingleBlockquote } from './single/Blockquote.vue'
---
+++ src/components/single/Excerpt.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import Vue, { VNode } from 'vue'
+export default /*#__PURE__*/Vue.extend({
+  computed: {
+    classesContainer(): string {
+      return this.$style.coreContainer
+    },
+    classesP(): string {
+      return this.$style.coreP
+    }
+  },
+  render (h): VNode {
+    return h(
+      'div',
+      {
+        class: this.classesContainer
+      },
+      [
+        h(
+          'p',
+          {
+            class: this.classesP
+          },
+          /**
+           * @slot Slot standar Vue
+           */
+          this.$slots.default
+        )
+      ]
+    )
+  }
+})
+</script>
+<style module lang="postcss" scoped>
+  .coreContainer {
+    @apply mx-auto w-full max-w-md;
+  }
+
+  .coreP {
+    @apply font-system text-center leading-loose;
+  }
+
+</style>
