94243bc44ff941b0c275a946adbcea29d2da98a2
naomiaputri <naomi.aryatiputri@kompas.com>
2021-09-22 14:19:53 +0700

    h1, h2, h3

---
+++ src/components/single/H6.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import Vue, { VNode } from 'vue'
+export default /*#__PURE__*/Vue.extend({
+  computed: {
+    classes () {
+      return [
+        this.$style.core
+      ]
+    }
+  },
+  render (h): VNode {
+    return h(
+      'h6',
+      {
+        class: this.classes
+      },
+      this.$slots.default
+    )
+  }
+})
+</script>
+<style module lang="postcss" scoped>
+  .core {
+    @apply font-serif font-bold text-base text-grey-600;
+  }
+</style>
