4a5db94a8401a30f2e176b096fe219bd399a91ae
Yosef Yudha Wijaya <yosef@yudhawijaya.com>
2021-11-17 12:03:35 +0700

    Update H3.vue

--- src/components/single/H3.vue
+++ src/components/single/H3.vue
@@ -1,27 +1,23 @@
 <script lang="ts">
-import Vue, { VNode } from 'vue'
-export default /*#__PURE__*/Vue.extend({
-  computed: {
-    classes () {
-      return [
-        this.$style.core
-      ]
-    }
-  },
-  render (h): VNode {
+import Vue, { CreateElement } from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class KsmSingleH2 extends Vue {
+  render (h: CreateElement) {
     return h(
       'h3',
       {
-        class: this.classes
+        class: this.$style.core
       },
       this.$slots.default
     )
   }
-})
+}
 </script>
 <style module lang="postcss" scoped>
   .core {
-    @apply font-serif font-bold text-grey-600;
+    @apply font-serif font-bold mx-auto text-grey-600 w-full max-w-md;
     font-size: 1.375rem; /* 22px; */
     line-height: 2rem;
   }
